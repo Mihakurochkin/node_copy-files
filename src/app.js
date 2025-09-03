d7f8d47853a1bd9e5d7468b167b99f7e51710c27
Mykhailo <kurochkinmy@gmail.com>
2025-09-03 14:22:42 +0300

    fix: handle fs errors

--- src/app.js
+++ src/app.js
@@ -2,37 +2,53 @@
 /* eslint-disable no-console */
 
 const fs = require('fs');
+const path = require('path');
 const [sourcePath, destinationPath] = process.argv.slice(2);
 
 function copyFile(source, destination) {
-  if (arguments.length === 1 || !source || !destination) {
+  if (arguments.length !== 2 || !source || !destination) {
     console.error('Invalid number of arguments');
 
     return;
   }
 
-  if (!fs.existsSync(source)) {
-    console.error('Source file does not exist');
+  try {
+    if (!fs.existsSync(source)) {
+      console.error('Source file does not exist');
+
+      return;
+    }
+  } catch (err) {
+    console.error('Error checking if source file exists:', err.message);
 
     return;
   }
 
-  if (source === destination) {
+  if (path.resolve(source) === path.resolve(destination)) {
     return;
   }
 
-  if (
-    fs.statSync(source).isDirectory() ||
-    (fs.existsSync(destination) && fs.statSync(destination).isDirectory())
-  ) {
-    console.error('Source or destination cannot be a directory');
+  try {
+    if (
+      fs.statSync(source).isDirectory() ||
+      (fs.existsSync(destination) && fs.statSync(destination).isDirectory())
+    ) {
+      console.error('Source or destination cannot be a directory');
+
+      return;
+    }
+  } catch (err) {
+    console.error(err.message);
 
     return;
   }
 
-  const sourceContent = fs.readFileSync(source, 'utf-8');
-
-  fs.writeFileSync(destination, sourceContent);
+  try {
+    fs.copyFileSync(source, destination);
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
 }
 
 copyFile(sourcePath, destinationPath);
